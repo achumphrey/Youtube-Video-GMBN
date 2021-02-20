614ab69d69036131f35956163a39e85902b3c482
AcHumphrey <AcHumphrey@example.com>
2021-02-20 21:50:47 +0000

    added comment list

--- app/src/main/java/com/example/youtubevideogmbn/ComentListFragment.kt
+++ app/src/main/java/com/example/youtubevideogmbn/ComentListFragment.kt
@@ -1,16 +1,26 @@
 package com.example.youtubevideogmbn
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import java.util.*
 
 
 /**
  * A simple [Fragment] subclass.
  */
 class ComentListFragment : Fragment() {
+
+    private var commentList = arrayListOf<String>()
+    lateinit var arrayAdapter: ArrayAdapter<*>
+    lateinit var commentListView: ListView
 
 
     override fun onCreateView(
@@ -21,4 +31,37 @@ class ComentListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_coment_list, container, false)
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        makeCommentList()
+
+        val view = view
+
+        commentListView = view?.findViewById(R.id.commentList)!!
+
+        arrayAdapter = ArrayAdapter(
+            context!!.applicationContext,
+            android.R.layout.simple_list_item_1,
+            commentList
+        )
+        commentListView.adapter = arrayAdapter
+
+        commentListView.setOnItemClickListener {
+                parent,
+                view,
+                position,
+                id ->
+            Toast.makeText(
+                context!!.applicationContext,
+                commentList[position],
+                Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    private fun makeCommentList() {
+        for (i in 1..10)
+            commentList.add("Comment " + i + " by Author " + i)
+    }
 }
